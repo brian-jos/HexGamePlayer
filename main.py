b53fc55eeaf687e4d2cb3b58ec3790c1dff5d193
Brian Rogador <brianjos@ualberta.ca>
2020-09-13 15:34:07 -0600

    transferred project from repl.it to local storage

--- main.py
+++ main.py
@@ -1,16 +1,80 @@
-# This is a sample Python script.
+def displayLegend():
+  print("\n===========================")
+  print("_ : Empty")
+  print("x : Black (Letter Axis)")
+  print("o : White (Number Axis)")
+  print("===========================")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def inputDimensions():
+  rows = int(input("Enter # of rows (max 99): "))
+  columns = int(input("Enter # of columns (max 26): "))
+  return rows, columns
 
+def generateBoard(rows, columns):
+  row = list()
+  for i in range(columns):
+    row.append(0) # represents empty cell
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+  board = list()
+  for i in range(rows):
+    board.append(list(row)) # new list instead of reference
+  return board
 
+def displayBoard(board, rows, columns):
+  print()
+  for y in range(rows + 2): # considers letter axis (+2)
+    if (y < 1 or y > rows): # first or last row
+      print("  ", end = "")
+    if (y < 10): # single digit row
+      print(" " * y, end = "")
+    else: # double digit row
+      print(" " * (y - 1), end = "")
+    letterAxis = "A" # starting letter of letter axis
+    for x in range(columns + 2): # considers number axis (+2)
+      if (y < 1 or y > rows): # first or last row
+        print(letterAxis, end = " ") # print letter axis
+        letterAxis = chr(ord(letterAxis) + 1)
+        if (x > columns - 2): # letters won't exceed number of columns
+          break
+      # print number axis
+      elif (x < 1): # first column
+        print(y, end = "")
+      elif (x > columns): # last column
+        print("\\" + str(y), end = "")
+      else: # position is on the board
+        if (board[y - 1][x - 1] == 1):
+          print("\\x", end = "") # display black
+        elif (board[y - 1][x - 1] == 2):
+          print("\\o", end = "") # display white
+        else:
+          print("\\_", end = "") # display empty
+    print()
 
-# Press the green button in the gutter to run the script.
+def enterCommand(currentTurn, board):
+  if (currentTurn == 0):
+    displayLegend()
+    command = input("Black's turn to move: ").upper()
+  else:
+    displayLegend()
+    command = input("White's turn to move: ").upper()
+
+  row = ord(command[0]) - 65
+  column = int(command[1:]) - 1
+
+  if (currentTurn == 0):
+    board[column][row] = 1
+  else:
+    board[column][row] = 2
+
+  return board
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    rows, columns = inputDimensions()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    board = generateBoard(rows, columns)
+
+    currentTurn = 0
+    while 1:
+        displayBoard(board, rows, columns)
+        board = enterCommand(currentTurn, board)
+        currentTurn = (currentTurn + 1) % 2;
