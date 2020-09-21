1a3d5583ec30ce7a32a7d0aa21339f7e28b25299
Brian Rogador <brianjos@ualberta.ca>
2020-09-21 17:56:27 -0600

    merged constants.py contents into main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import constants
 import sys
 
 """
@@ -9,6 +8,12 @@ TO DO:
 - clean up code and find bugs
 """
 
+# constants
+EMPTY = 0
+BLACK = 1
+WHITE = 2
+MAX_ROWS = 99
+MAX_COLUMNS = 26
 
 class HexBoard:
 
@@ -26,7 +31,7 @@ class HexBoard:
         # create 2D array from rows and columns
         row_array = list()
         for x in range(self.columns):  # fill initial row with 0s
-            row_array.append(constants.EMPTY)
+            row_array.append(EMPTY)
         for y in range(self.rows):  # fill board_array with rows
             # list(row_array) uses new list instead of a reference
             self.board_array.append(list(row_array))
@@ -64,9 +69,9 @@ class HexBoard:
                 elif x > self.columns:  # last column
                     print("\\", end=str(y))
                 else:  # position is on the board
-                    if self.board_array[y - 1][x - 1] == constants.BLACK:
+                    if self.board_array[y - 1][x - 1] == BLACK:
                         print(end="\\x")  # print black tile
-                    elif self.board_array[y - 1][x - 1] == constants.WHITE:
+                    elif self.board_array[y - 1][x - 1] == WHITE:
                         print(end="\\o")  # print white tile
                     else:
                         print(end="\\_")  # print empty tile
@@ -86,7 +91,7 @@ class HexBoard:
         replaces the last position in history with an empty stone
         """
         row, column, stone = self.history[-1]
-        self.board_array[row][column] = constants.EMPTY
+        self.board_array[row][column] = EMPTY
         self.history.pop()
 
     def valid_placement(self, row, column):
@@ -95,7 +100,7 @@ class HexBoard:
         checks if specified location on the board is empty and within bounds
         """
         if 0 <= row < self.rows and 0 <= column < self.columns \
-                and self.board_array[row][column] == constants.EMPTY:
+                and self.board_array[row][column] == EMPTY:
             return True
         return False
 
@@ -139,7 +144,7 @@ def input_command(current_turn, board):  # TO DO: reformat this into a cleaner w
     - the color of the stone depends on whose turn it is
     - typing "quit" will exit the program
     """
-    if current_turn == constants.BLACK:
+    if current_turn == BLACK:
         print("Black's turn.")
     else:
         print("White's turn.")
@@ -154,9 +159,9 @@ def input_command(current_turn, board):  # TO DO: reformat this into a cleaner w
             continue
         if command == "UNDO" and len(board.history) > 0:
             board.undo_placement()
-            if current_turn == constants.BLACK:  # alternate turns
-                return constants.WHITE
-            return constants.BLACK
+            if current_turn == BLACK:  # alternate turns
+                return WHITE
+            return BLACK
         # format: A00 (LETTER NUMBER)
         if not (len(command) > 1 and command[0].isalpha() and command[1:].isdigit()):
             continue  # wrong format
@@ -167,19 +172,19 @@ def input_command(current_turn, board):  # TO DO: reformat this into a cleaner w
         board.place_stone(row, column, current_turn)
         break
 
-    if current_turn == constants.BLACK:  # alternate turns
-        return constants.WHITE
-    return constants.BLACK
+    if current_turn == BLACK:  # alternate turns
+        return WHITE
+    return BLACK
 
 
 if __name__ == "__main__":
     print("\nMinimum requirement: Python 3.0")
-    rows = input_dimension("rows", constants.MAX_ROWS)
-    columns = input_dimension("columns", constants.MAX_COLUMNS)
+    rows = input_dimension("rows", MAX_ROWS)
+    columns = input_dimension("columns", MAX_COLUMNS)
     board = HexBoard(rows, columns)
 
     # alternate turns and loop
-    current_turn = constants.BLACK
+    current_turn = BLACK
     while True:
         board.display_board()
         display_guide()
